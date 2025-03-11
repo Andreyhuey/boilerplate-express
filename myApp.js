abff1b5ee1809c2ad86dee80fdb1f6351212ccb1
andreyhuey <andreyhuey777@gmail.com>
2025-03-11 23:33:38 +0100

    feat(myApp.js): add body-parser middleware to parse URL-encoded data

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 let express = require("express");
+let bodyParser = require("body-parser");
+
 let app = express();
 
 console.log("Hello World");
+
+// Middleware to parse URL-encoded data
+app.use(bodyParser.urlencoded({ extended: false }));
 
 module.exports = app;
