bc7669f533c0c86eb6570045d8e99d629f878dcb
andreyhuey <andreyhuey777@gmail.com>
2025-03-11 22:25:59 +0100

    feat(index.js): add JSON endpoint to serve a greeting message

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 const port = process.env.PORT || 3000;
